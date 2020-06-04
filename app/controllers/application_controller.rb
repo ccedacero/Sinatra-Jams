9add90b1e4857c8b40d2668faa97fe4104543bd9
ccedacero <cristian.cedacero@gmail.com>
2020-06-04 16:42:26 -0400

    complete create & read

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,31 @@
-require './config/environment'
+require "./config/environment"
 
 class ApplicationController < Sinatra::Base
-
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+    set :public_folder, "public"
+    set :views, "app/views"
   end
 
   get "/" do
     erb :welcome
   end
 
+  get "/artists" do
+    @artists = Artist.all
+    erb :index
+  end
+
+  get "/artists/new" do
+    erb :new
+  end
+
+  get "/artists/:id" do
+    @artist = Artist.find(params[:id])
+    erb :show
+  end
+
+  post "/artists" do
+    artist = Artist.create(params[:artist])
+    redirect "/artists/#{artist.id}"
+  end
 end
